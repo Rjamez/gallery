a3590f061a2d6a1f62b50446b0ff815e7171d41b
Robin <robin.adhola@student.moringaschool.com>
2025-06-20 14:20:29 +0300

    Milestone 4: Slack notification and heading

--- server.js
+++ server.js
@@ -11,46 +11,32 @@ let image = require('./routes/image');
 // Initializing the app
 const app = express();
 
-// connecting the database
+// Connecting the database
+const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env];
 
-const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
-    if (err) {
-        console.log(err)
-    }else{
-        console.log(`Connected to Database: ${MONGODB_URI}`)
-    }
-});
-
-// test if the database has connected successfully
-// let db = mongoose.connection;
-// db.once('open', ()=>{
-//     console.log('Database connected successfully')
-// })
-
-
-
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log(`Connected to Database: ${MONGODB_URI}`);
+  })
+  .catch(err => {
+    console.error('Database connection error:', err);
+  });
 
 // View Engine
 app.set('view engine', 'ejs');
 
-// Set up the public folder;
+// Set up the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // body parser middleware
-app.use(express.json())
-
+app.use(express.json());
 
 app.use('/', index);
 app.use('/image', image);
 
-
-
- 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,() =>{
-    console.log(`Server is listening at http://localhost:${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server is listening at http://localhost:${PORT}`);
 });
-
 
 module.exports = app;
