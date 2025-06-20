442e000ee16fe5e9d69f0acee3c17794b1152ac5
Robin <robin.adhola@student.moringaschool.com>
2025-06-20 14:36:37 +0300

    Milestone 4: Slack notification and heading

--- routes/index.js
+++ routes/index.js
@@ -2,54 +2,42 @@ const express = require('express');
 const router = express.Router();
 const uuid = require('uuid');
 let upload = require('./upload');
-const url = require('url')
+const url = require('url');
 let Image = require('../models/images');
 
+// Fix for the GET route - using async/await
+router.get('/', async (req, res, next) => {
+  try {
+    const images = await Image.find({});
+    res.render('index', { images: images, msg: req.query.msg });
+  } catch (err) {
+    next(err);
+  }
+});
 
+router.post('/upload', (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      res.redirect(`/?msg=${err}`);
+    } else {
+      console.log(req.file);
+      if (req.file == undefined) {
+        res.redirect('/?msg=Error: No file selected!');
+      } else {
+        // create new image
+        let newImage = new Image({
+          name: req.file.filename,
+          size: req.file.size,
+          path: 'images/' + req.file.filename,
+        });
 
+        // save the uploaded image to the database
+        newImage.save();
 
-router.get('/', (req,res)=>{
-    
-    Image.find({}, function(err, images){
-        // console.log(images)
-        if (err) console.log(err);
-        res.render('index',{images:images, msg: req.query.msg })
-    })
-})
-
-router.post('/upload', (req, res)=>{
-    upload(req,res, (err)=>{
-        if (err){
-            res.redirect(`/?msg=${err}`);
-        }else{
-            console.log(req.file);
-            // res.send("test");
-            if (req.file == undefined){
-                res.redirect('/?msg=Error: No file selcted!');
-            }else{
-                // const imageObj = {
-                //     id: uuid.v4(),
-                //     name: req.file.filename,
-                //     path: 'images/' + req.file.filename
-                // }
-                // db.push(imageObj);
-                // console.log(db);
-
-                // create new image
-                let newImage = new Image({
-                    name: req.file.filename,
-                    size: req.file.size,
-                    path: 'images/' + req.file.filename
-                })
-
-                // save the uploaded image to the database
-                newImage.save()
-
-                
-                res.redirect('/?msg=File uploaded successfully');
-            }
-        }
-    })
-})
+        res.redirect('/?msg=File uploaded successfully');
+      }
+    }
+  });
+});
 
 module.exports = router;
